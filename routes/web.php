488582e65ba3926b62c67ed7a3cadbb4a6064b85
Jumana Obeid <color.ui>
2017-04-25 13:24:28 +0300

    hashed the route to log viewer

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::resource('tasks','TaskController');
 
 
 if(config('app.env') == 'local') {
-    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+    #Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 }
 
 /**
