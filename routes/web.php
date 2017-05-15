e5dc4600840d40fef97e9047a0733eb2092eed33
Jumana Obeid <color.ui>
2017-05-15 22:08:32 +0300

    Fixed date validation

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,3 @@ Route::resource('locations','LocationController',['except' => 'destroy']);
 if(config('app.env') == 'local') {
     #Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 }
-
-/**
-* Practice
-*/
-Route::any('/practice/{n?}', 'PracticeController@index');
